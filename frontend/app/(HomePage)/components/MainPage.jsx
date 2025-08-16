cacf88131ee1a87c3caf94f9044a14074f729996
Dracgamer5643 <supermee5643@gmail.com>
2025-08-16 12:57:52 +0530

    Connected with vercel

--- frontend/app/(HomePage)/components/MainPage.jsx
+++ frontend/app/(HomePage)/components/MainPage.jsx
@@ -9,20 +9,26 @@ export default function MainPage() {
     const [dbs, setDbs] = React.useState([]);
     const [geminiMessage, setGeminiMessage] = React.useState("");
     const [dbFilePath, setDbFilePath] = React.useState("");
-    const token = localStorage.getItem('authToken');
-
-    React.useEffect(()=>{
-        if (!token) {
-            const newToken = crypto.randomUUID();
-            localStorage.setItem('authToken', newToken);
+    const [token, setToken] = React.useState("");
+    React.useEffect(() => {
+        if (typeof window !== "undefined") {
+            const token = localStorage.getItem("token");
+            if (!token) {
+                const newToken = crypto.randomUUID();
+                localStorage.setItem('authToken', newToken);
+                setToken(newToken);
+            }
+            else{
+                setToken(token);
+            }
         }
-    },[])
+    }, [])
 
     return (
         <div className='w-[90%] m-auto border-l border-r border-indigo-400 min-h-screen'>
             <Header />
-            <PromptForm prompt={prompt} setPrompt={setPrompt} setDbs={setDbs} setGeminiMessage={setGeminiMessage} setDbFilePath={setDbFilePath} token={token}/>
-            <Output prompt={prompt} dbs={dbs} geminiMessage={geminiMessage} dbFilePath={dbFilePath} token={token}/>
+            <PromptForm prompt={prompt} setPrompt={setPrompt} setDbs={setDbs} setGeminiMessage={setGeminiMessage} setDbFilePath={setDbFilePath} token={token} />
+            <Output prompt={prompt} dbs={dbs} geminiMessage={geminiMessage} dbFilePath={dbFilePath} token={token} />
         </div>
     )
 }
